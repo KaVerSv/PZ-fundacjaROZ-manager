33ff5c04ddfabbe91bc498aced4ea30c16d0450e
ZakharSytoi <zakharsytoi@gmail.com>
2024-03-17 14:46:24 +0100

    bg moved 80px up

--- frontend/src/components/Header/Top.tsx
+++ frontend/src/components/Header/Top.tsx
@@ -5,7 +5,7 @@ type props = {
 }
 function Top({children, headerHeight} : props) {
     return (
-        <div className="bg-cover bg-center bg-clip-border bg-[url('src/components/Header/assets/ROZ_background_Top.jpeg')]" style={{paddingBottom:`${headerHeight}px`, backgroundPosition:'center 0px'}}>
+        <div className="bg-cover bg-center bg-clip-border bg-[url('src/components/Header/assets/ROZ_background_Top.jpeg')]" style={{paddingBottom:`${headerHeight}px`, backgroundPosition:'center -80px'}}>
             {children}
         </div>
     );
