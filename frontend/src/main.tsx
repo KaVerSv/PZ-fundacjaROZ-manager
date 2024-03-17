6fb3fa70787aa5c88ffb443efe94b3300ab18dbd
ZakharSytoi <zakharsytoi@gmail.com>
2024-03-17 14:43:27 +0100

    added addchild route

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,20 +3,21 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import {createBrowserRouter, RouterProvider} from "react-router-dom";
 import Home from "./pages/Home.tsx";
+import ChildCreationPage from "./pages/ChildCreationPage.tsx";
 
 const router = createBrowserRouter([
     {
         path: "/",
-        element: <Home />,
+        element: <Home/>,
     },
     {
-        path: "/hello",
-        element: <h1>Hello React Route!!</h1>,
+        path: "/addchild",
+        element: <ChildCreationPage/>,
     }
 ]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-      <RouterProvider router={router} />
-  </React.StrictMode>,
+    <React.StrictMode>
+        <RouterProvider router={router}/>
+    </React.StrictMode>,
 )
