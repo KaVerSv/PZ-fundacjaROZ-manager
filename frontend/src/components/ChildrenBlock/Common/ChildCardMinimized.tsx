7cf05743c485665ca44ae075e7edb35a5d8c115e
ZakharSytoi <zakharsytoi@gmail.com>
2024-03-17 13:52:31 +0100

    child card parametrized

--- frontend/src/components/ChildrenBlock/Common/ChildCardMinimized.tsx
+++ frontend/src/components/ChildrenBlock/Common/ChildCardMinimized.tsx
@@ -1,8 +1,20 @@
-function ChildCardMinimized() {
+import {ChildModelMinimized} from "../../../models/ChildModelMinimized.tsx";
+
+interface ChildProps {
+    isArchived: boolean,
+    child: ChildModelMinimized
+}
+
+// 'src/components/ChildrenBlock/2.jpg'
+function ChildCardMinimized(props: ChildProps) {
+    let optionalStyles = props.isArchived ?
+        `bg-main_red hover:bg-red_selected` :
+        `bg-main_grey hover:bg-grey_selected`;
     return (
-        <a href='/' className='ml-10 flex max-w-72 h-16 bg-main_red rounded-xl hover:bg-red-selected hover:cursor-pointer'>
-            <img className='ml-5 p-0.5 rounded-full' src='src/components/ChildrenBlock/2.jpg'/>
-            <span className='flex items-center ml-2 text-main_white'>Jan Kowalski</span>
+        <a href='/' className={`mx-auto my-2 sm:m-0 flex max-w-72 h-16 rounded-xl hover:cursor-pointer ${optionalStyles}`}>
+            <img className='ml-5 p-0.5 rounded-full' src={props.child.childPhotoLink}/>
+            <span
+                className='flex items-center ml-2 text-main_white'>{props.child.childName} {props.child.childSurname}</span>
         </a>
     );
 }
