8a904ef47c5983f714917b64da6f62df77d0b9b8
ZakharSytoi <zakharsytoi@gmail.com>
2024-03-18 17:01:13 +0100

    show errors properly with date and select

--- frontend/src/components/ChildCreationForm/InputWrapper.tsx
+++ frontend/src/components/ChildCreationForm/InputWrapper.tsx
@@ -18,7 +18,7 @@ const InputWrapper = ({children, labelFor, labelNote, shift, alwaysShowLabel, er
         setInputValue(event.target.value.trim()); // Update input value state
     };
     return (
-        <div className={`flex flex-col mx-5 my-3 relative mb-3 mt-6`} onChange={handleChange}>
+        <div className={`flex flex-col ${alwaysShowLabel? 'min-w-72':''} mx-5 my-3 relative mb-3 mt-6`} onChange={handleChange}>
             <LabelForInput labelFor={labelFor} labelNote={labelNote} isDirty={alwaysShowLabel || !!inputValue} shift={!!shift? shift : [0,0]}/>
             {children}
             {error && <div
