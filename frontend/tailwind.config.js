7b09b46bacccae67d911f3f71114768f5e257f7f
ZakharSytoi <zakharsytoi@gmail.com>
2024-03-18 09:47:13 +0100

    color theme now extend standard instead of substituting

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -5,15 +5,15 @@ module.exports = {
         "./src/**/*.{js,ts,jsx,tsx}",
     ],
     theme: {
-        colors: {
-            'main_red': '#77142B',
-            'red_selected': '#9b0014',
-            'main_grey': '#666666',
-            'grey_selected': '#9b9b9b',
-            'main_white': '#FFFFFF',
-            'main_black': '#000000'
-        },
         extend: {
+            colors: {
+                'main_red': '#77142B',
+                'red_selected': '#9b0014',
+                'main_grey': '#666666',
+                'grey_selected': '#9b9b9b',
+                'main_white': '#FFFFFF',
+                'main_black': '#000000'
+            },
             fontFamily: {
                 oswald: ['Oswald', 'sans-serif'],
                 roboto: ['Roboto Condensed', 'sans-serif']
