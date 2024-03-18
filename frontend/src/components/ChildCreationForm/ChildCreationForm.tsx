3650c136211f681e5975bc108057443eba61b7e6
ZakharSytoi <zakharsytoi@gmail.com>
2024-03-18 15:39:31 +0100

    input blocks substituted by FormInput's

--- frontend/src/components/ChildCreationForm/ChildCreationForm.tsx
+++ frontend/src/components/ChildCreationForm/ChildCreationForm.tsx
@@ -1,6 +1,7 @@
 import {SubmitHandler, useForm} from "react-hook-form";
 import WidthWrapper from "../wrappers/WidthWrapper.tsx";
 import InputWrapper from "./InputWrapper.tsx";
+import FormInput from "./FormInput.tsx";
 
 interface FormData {
     pesel: string;
@@ -25,15 +26,20 @@ enum GenderEnum {
 
 
 function ChildCreationForm() {
-    const {register, handleSubmit} = useForm<FormData>()
+    const {register, handleSubmit, formState: {errors}} = useForm<FormData>({
+        mode: 'onChange'
+
+    })
     const onSubmit: SubmitHandler<FormData> = (data) => {
         console.log(data)
     };
-
+    const validateGender = (value: GenderEnum) => {
+        return value === GenderEnum.notDefined ? 'Pole Obowiązkowe' : true;
+    };
     return (
         <div className='mt-3'>
             <WidthWrapper>
-                <form className='flex flex-col items-center border-main_red border-r-4'
+                <form className='flex flex-col items-center border-main_red border-4 p-2 rounded-2xl'
                       onSubmit={handleSubmit(onSubmit)}>
                     <div className='flex flex-col lg:flex-row'>
                         <div className='flex items-center'>
@@ -46,27 +52,17 @@ function ChildCreationForm() {
                         </div>
                         <div className='flex flex-col'>
                             <div className='flex flex-col sm:grid sm:grid-cols-2 xl:flex xl:flex-row'>
-                                <InputWrapper labelFor="firstName" labelNote="Imie">
-                                    <input
-                                        className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-                                        type="text" id="firstName"
-                                        placeholder="Imie" {...register('firstName')} />
-                                </InputWrapper>
-                                <InputWrapper labelFor="lastName" labelNote="Drugie Imie">
-                                    <input
-                                        className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-                                        type="text" id="lastName"
-                                        placeholder="Drugie Imie" {...register('secondName')} />
-                                </InputWrapper>
-                                <InputWrapper labelFor="surname" labelNote="Nazwisko">
-                                    <input
-                                        className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-                                        type="text" id="surname" placeholder="Nazwisko" {...register('surname')} />
-                                </InputWrapper>
-                                <InputWrapper labelFor="gender" labelNote="Płeć">
+                                <FormInput name={'firstName'} type={'text'} label={'Imie'} register={register}
+                                           rules={{required: 'Pole Obowiązkowe'}} error={errors.firstName}/>
+                                <FormInput name={"secondName"} type={"text"} label={"Drugie Imie"} register={register}
+                                           rules={{required: 'Pole Obowiązkowe'}} error={errors.secondName}/>
+                                <FormInput name={"surname"} type={"text"} label={"Nazwisko"} register={register}
+                                           rules={{required: 'Pole Obowiązkowe'}} error={errors.surname}/>
+                                <InputWrapper labelFor="gender" labelNote="Płeć" error={errors.gender}>
                                     <select
                                         className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-                                        id="gender" {...register('gender')} defaultValue={GenderEnum.notDefined}>
+                                        id="gender" {...register('gender', {required: true, validate: validateGender})}
+                                        defaultValue={GenderEnum.notDefined}>
                                         <option disabled hidden value={GenderEnum.notDefined}>Płeć</option>
                                         <option value={GenderEnum.male}>Mężczyzna</option>
                                         <option value={GenderEnum.female}>Kobieta</option>
@@ -82,62 +78,40 @@ function ChildCreationForm() {
                                 </InputWrapper>
                             </div>
                             <div className='flex flex-col sm:grid sm:grid-cols-2 xl:flex xl:flex-row'>
-                                <InputWrapper labelFor="birthDate" labelNote="Data urodzenia" alwaysShowLabel={true}>
-                                    <input
-                                        className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-                                        type="date" id="birthDate"
-                                        placeholder="Data urodzenia" {...register('birthDate')} />
-                                </InputWrapper>
-                                <InputWrapper labelFor="birthplace" labelNote="Miejsce urodzenia">
-                                    <input
-                                        className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-                                        type="text" id="birthplace"
-                                        placeholder="Miejsce urodzenia" {...register('birthplace')} />
-                                </InputWrapper>
-                                <InputWrapper labelFor="pesel" labelNote="PESEL">
-                                    <input
-                                        className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-                                        type="text" id="pesel" placeholder="PESEL" {...register('pesel')} />
-                                </InputWrapper>
+                                <FormInput name={"birthDate"} type={"date"} label={"Data urodzenia"} register={register}
+                                           rules={{required: 'Pole Obowiązkowe'}} error={errors.birthDate}/>
+                                <FormInput name={"birthplace"} type={"text"} label={"Miejsce urodzenia"}
+                                           register={register} rules={{required: 'Pole Obowiązkowe'}}
+                                           error={errors.birthplace}/>
+                                <FormInput name={"pesel"} type={"text"} label={"PESEL"} register={register}
+                                           rules={{required: 'Pole Obowiązkowe'}} error={errors.pesel}/>
                             </div>
                             <div className='flex flex-col'>
-                                <InputWrapper labelFor="Adres zamieszkania" labelNote="Residential Address">
-                                    <input
-                                        className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-                                        type="text" id="residentialAddress"
-                                        placeholder="Residential Address" {...register('residentialAddress')} />
-                                </InputWrapper>
-                                <InputWrapper labelFor="registeredAddress" labelNote="Adres zameldowania">
-                                    <input
-                                        className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-                                        type="text" id="registeredAddress"
-                                        placeholder="Adres zameldowania" {...register('registeredAddress')} />
-                                </InputWrapper>
+                                <FormInput name={"residentialAddress"} type={"text"} label={"Adres zamieszkania"}
+                                           register={register} rules={{required: 'Pole Obowiązkowe'}}
+                                           error={errors.residentialAddress}/>
+                                <FormInput name={"registeredAddress"} type={"text"} label={"Adres zameldowania"}
+                                           register={register} rules={{required: 'Pole Obowiązkowe'}}
+                                           error={errors.registeredAddress}/>
                             </div>
                             <div className='flex flex-col sm:flex-row'>
-                                <InputWrapper labelFor="admissionDate" labelNote="Data przyjęcia" alwaysShowLabel={true}>
-                                    <input
-                                        className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-                                        type="date" id="admissionDate"
-                                        placeholder="Admission Date" {...register('admissionDate')} />
-                                </InputWrapper>
-                                <InputWrapper labelFor="leavingDate" labelNote="Data opuszczenia" alwaysShowLabel={true}>
-                                    <input
-                                        className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-                                        type="date" id="leavingDate"
-                                        placeholder="Leaving Date" {...register('leavingDate')} />
-                                </InputWrapper>
+                                <FormInput name={"admissionDate"} type={"date"} label={"Data przyjęcia"}
+                                           register={register} rules={{required: 'Pole Obowiązkowe'}}
+                                           error={errors.admissionDate}/>
+                                <FormInput name={"leavingDate"} type={"date"} label={"Data opuszczenia"}
+                                           register={register} rules={{required: 'Pole Obowiązkowe'}}
+                                           error={errors.leavingDate}/>
                             </div>
                         </div>
                     </div>
                     <div>
                         <button
-                            type="submit">Submit
+                            className='mt-3 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded'
+                            type="submit">Dodaj dziecko
                         </button>
                     </div>
                 </form>
             </WidthWrapper>
-
         </div>
     );
 }
