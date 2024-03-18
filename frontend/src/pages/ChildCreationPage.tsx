7ad8b651d56f1369ffafe22d406fec3569fda74b
ZakharSytoi <zakharsytoi@gmail.com>
2024-03-18 15:40:17 +0100

    child creation page template

--- frontend/src/pages/ChildCreationPage.tsx
+++ frontend/src/pages/ChildCreationPage.tsx
@@ -1,6 +1,8 @@
 import HeightWrapper from "../components/wrappers/HeightWrapper.tsx";
 import Header from "../components/Header/Header.tsx";
 import Top from "../components/Header/Top.tsx";
+import ChildCreationForm from "../components/ChildCreationForm/ChildCreationForm.tsx";
+
 
 function ChildCreationPage() {
     return (
@@ -8,6 +10,7 @@ function ChildCreationPage() {
             <Top headerHeight={350}>
                 <Header/>
             </Top>
+            <ChildCreationForm/>
         </HeightWrapper>
     );
 }
