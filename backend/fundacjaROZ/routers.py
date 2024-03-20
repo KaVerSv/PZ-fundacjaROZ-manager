c3cf124bc793379dfcfc7bb7011804548cee30b9
zywczak <123404938+zywczak@users.noreply.github.com>
2024-03-20 21:20:48 +0100

    routers
    name change

--- backend/fundacjaROZ/routers.py
+++ backend/fundacjaROZ/routers.py
@@ -3,6 +3,6 @@ from rest_framework.routers import DefaultRouter
 from .views import AddChildAPIView, AddRelativeAPIView,DispayChildren
 
 router = DefaultRouter()
-router.register(r'add_children', AddChildAPIView)
+router.register(r'children', AddChildAPIView)
 router.register(r'relatives', AddRelativeAPIView)
 # router.register(r'child', DispayChildren, basename='child')
