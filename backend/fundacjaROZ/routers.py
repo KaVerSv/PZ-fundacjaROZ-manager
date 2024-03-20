46639d1b1ffa70373a2840db58d0de9b8b0cff12
Kondro571 <kondziot2000@gmail.com>
2024-03-20 21:27:56 +0100

    current,arch

--- backend/fundacjaROZ/routers.py
+++ backend/fundacjaROZ/routers.py
@@ -3,7 +3,7 @@ from rest_framework.routers import DefaultRouter
 from .views import *
 
 router = DefaultRouter()
-router.register(r'add_children', AddChildAPIView)
+router.register(r'children', AddChildAPIView)
 router.register(r'relatives', AddRelativeAPIView)
 router.register(r'children/current', DispayChildrenCurrent, basename='child')
 router.register(r'children/archival', DispayChildrenArchival, basename='child2')
