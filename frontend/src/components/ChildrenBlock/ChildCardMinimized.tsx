3ac217a7025e4b3b3444bf7a194f65ee4799f40a
ZakharSytoi <zakharsytoi@gmail.com>
2024-03-21 13:38:02 +0100

    child card now contains second name and redirects to child page

--- frontend/src/components/ChildrenBlock/ChildCardMinimized.tsx
+++ frontend/src/components/ChildrenBlock/ChildCardMinimized.tsx
@@ -1,4 +1,5 @@
 import {ChildModelMinimized} from "../../models/ChildModelMinimized.tsx";
+import {Link} from "react-router-dom";
 
 interface ChildProps {
     isArchived: boolean,
@@ -11,11 +12,11 @@ function ChildCardMinimized(props: ChildProps) {
         `bg-main_red hover:bg-red_selected` :
         `bg-main_grey hover:bg-grey_selected`;
     return (
-        <a href='/public' className={`mx-auto my-2 sm:m-0 flex max-w-72 h-16 rounded-xl hover:cursor-pointer ${optionalStyles}`}>
+        <Link to={`children/${props.child.id}`} className={`mx-auto my-2 sm:m-0 flex max-w-72 h-16 rounded-xl hover:cursor-pointer ${optionalStyles}`}>
             <img className='ml-5 p-0.5 rounded-full' src={props.child.childPhotoLink}/>
             <span
-                className='flex items-center ml-2 text-main_white'>{props.child.childName} {props.child.childSurname}</span>
-        </a>
+                className='flex items-center ml-2 text-main_white'>{props.child.childName} {props.child.childLastName} {props.child.childSurname}</span>
+        </Link>
     );
 }
 
