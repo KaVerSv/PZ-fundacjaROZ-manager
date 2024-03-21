1f2d2338cc5481935d0831b6aaa7482e3b27e72c
ZakharSytoi <zakharsytoi@gmail.com>
2024-03-21 13:39:24 +0100

    route for child page declared

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,6 +4,7 @@ import './index.css'
 import {createBrowserRouter, RouterProvider} from "react-router-dom";
 import Home from "./pages/Home.tsx";
 import ChildCreationPage from "./pages/ChildCreationPage.tsx";
+import ChildPage from "./pages/ChildPage.tsx";
 
 const router = createBrowserRouter([
     {
@@ -13,6 +14,10 @@ const router = createBrowserRouter([
     {
         path: "/addchild",
         element: <ChildCreationPage/>,
+    },
+    {
+        path: "/children/:id",
+        element: <ChildPage/>
     }
 ]);
 
