0dc09b526cf97ad3fa62c9f295eb9b280706316d
ZakharSytoi <zakharsytoi@gmail.com>
2024-03-22 12:55:51 +0100

    child page redesign

--- frontend/src/components/ChildPage/ChildCardMaximized.tsx
+++ frontend/src/components/ChildPage/ChildCardMaximized.tsx
@@ -14,30 +14,31 @@ function ChildCardMaximized(props: ChildCardProps) {
 
 
     return (
-        <div className='bg-main_red'>
+        <div className=''>
             <WidthWrapper>
-                <div className='flex flex-col sm:flex-row justify-center'>
-                    <div className='px-2 items-center'>
-                        <img className='px-1 py-2 w-56 sm:w-64' src={child.photoPath} alt='profilePhoto'/>
+                <div className='flex flex-col lg:flex-row gap-4 border-main_red border-4 p-4 rounded-2xl justify-center'>
+                    <div className='flex md:block justify-center'>
+                        <img className='pb-2 w-56 sm:w-72 lg:w-80 rounded-2xl' src={child.photoPath} alt='profilePhoto'/>
                     </div>
-                    <div className='flex flex-col '>
-                        <div className='flex flex-row gap-2'>
-                            <ChildInfoContainer note='' text={child.firstName}/>
-                            {child.secondName && <ChildInfoContainer note='' text={child.secondName}/>}
-                            <ChildInfoContainer note='' text={child.surname}/>
+                    <div className='flex flex-col mt-4 gap-7'>
+                        <div className='flex flex-row gap-1.5 mb-5'>
+                            <span className='text-2xl font-bold'>{child.firstName}</span>
+                            {child.secondName && <span className='text-2xl font-bold'>{child.secondName}</span>}
+                            <span className='text-2xl font-bold'>{child.surname}</span>
                         </div>
-                        <div>
+                        {/*<div>*/}
+
+                        {/*</div>*/}
+                        <div className='flex flex-col gap-7 sm:grid xl:gap-4 sm:grid-cols-2 xl:grid-cols-3'>
                             <ChildInfoContainer note='Płeć' text={child.gender === "male" ? 'Mężczyzna' : 'Kobieta'}/>
-                        </div>
-                        <div className='md:flex md:flex-row gap-4'>
                             <ChildInfoContainer note='Data urodzenia' text={child.birthDate}/>
                             <ChildInfoContainer note='Miejsce urodzeina' text={child.birthPlace}/>
                         </div>
-                        <div>
+                        <div className='flex flex-col gap-7'>
                             <ChildInfoContainer note='Adres zamieszkania' text={child.residentialAddress}/>
                             <ChildInfoContainer note='Adres zamieldowania' text={child.registeredAddress}/>
                         </div>
-                        <div className='md:flex md:flex-row gap-4'>
+                        <div className='flex flex-col sm:flex-row gap-7'>
                             <ChildInfoContainer note='Data przyjęcia' text={child.admissionDate}/>
                             <ChildInfoContainer note='Data opuszczenia' text={child.leavingDate}/>
                         </div>
--- frontend/src/components/ChildPage/ChildInfoContainer.tsx
+++ frontend/src/components/ChildPage/ChildInfoContainer.tsx
@@ -5,9 +5,10 @@ interface InfoProps {
 
 function ChildInfoContainer(props: InfoProps) {
     return (
-        <div className='flex flex-row gap-2 items-end'>
-            {props.note && <span className='text-main_white text-md'>{props.note}:</span>}
-            <span className='text-main_white text-lg'>{props.text}</span>
+        <div className='flex flex-col relative min-w-56 sm:min-w-56 md:min-w-72'>
+            {props.note && <span className={`tracking-wide text-gray-700 absolute font-bold mb-2 bottom-10 left-1 text-sm`}>
+                {props.note}:</span>}
+            <span className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight">{props.text}</span>
         </div>
     );
 }
