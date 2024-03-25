036687a7eeae78a27e7f369061e3bf52e1ecf3dc
zywczak <123404938+zywczak@users.noreply.github.com>
2024-03-25 21:48:10 +0100

    authentication works but must be updated
    to beter working authentication must be changed ModelViewSet to ApiView in classes

--- backend/fundacjaROZ/urls.py
+++ backend/fundacjaROZ/urls.py
@@ -1,9 +1,10 @@
-# from django.urls import path
-# from fundacjaROZ.views import UserRegistrationAPIView, UserLoginAPIView, UserViewAPI, UserLogoutViewAPI
+from django.urls import path
+from .views import UserRegistrationAPIView, UserLoginAPIView, UserViewAPI, UserLogoutViewAPI
 
-# urlpatterns = [
-# 	path('api/user/register/', UserRegistrationAPIView.as_view()),
-# 	path('api/user/login/', UserLoginAPIView.as_view()),
-# 	path('api/user/', UserViewAPI.as_view()),
-# 	path('api/user/logout/', UserLogoutViewAPI.as_view()),
-# ]
+
+urlpatterns = [
+	path('user/register/', UserRegistrationAPIView.as_view()),
+	path('user/login/', UserLoginAPIView.as_view()),
+	path('user/', UserViewAPI.as_view()),
+	path('user/logout/', UserLogoutViewAPI.as_view()),
+]
