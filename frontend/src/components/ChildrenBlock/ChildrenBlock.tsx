0261b401905ecfbbc5be1bf4345abdc1c46bf46e
ZakharSytoi <zakharsytoi@gmail.com>
2024-03-30 16:18:45 +0100

    main children block connected to backend

--- frontend/src/components/ChildrenBlock/ChildrenBlock.tsx
+++ frontend/src/components/ChildrenBlock/ChildrenBlock.tsx
@@ -2,12 +2,15 @@ import ChildCardMinimized from "./ChildCardMinimized.tsx";
 import WidthWrapper from "../wrappers/WidthWrapper.tsx";
 import React from "react";
 import CurrentBlockHeader from "./childBlockHeaders/CurrentBlockHeader.tsx";
+import useSWR from "swr";
 import {ChildModelMinimized} from "../../models/ChildModelMinimized.tsx";
+import {BASE_API_URL} from "../../api/contst.ts";
 
 interface ChildBlockProps {
     header: React.ReactNode
 }
 
+/*
 export const currentChildren: ChildModelMinimized[] = [
     {
         id: "1",
@@ -52,26 +55,32 @@ export const currentChildren: ChildModelMinimized[] = [
         childPhotoLink: 'src/assets/2.jpg'
     },
 ];
+*/
 
 function ChildrenBlock(props: ChildBlockProps) {
+    const fetcher: (url: string) => Promise<ChildModelMinimized[]> = async (url) => {
+        const response = await fetch(url);
+        if (!response.ok) {
+            throw new Error(`API request failed with status ${response.status}`);
+        }
+        return response.json();
+    };
+    const { data, error, isLoading } = useSWR<ChildModelMinimized[]>(BASE_API_URL +
+        `/children/${React.isValidElement(props.header) && props.header.type === CurrentBlockHeader ? 'archival/' : 'current/'}`, fetcher);
+    if (error) return <div>failed to load</div>
+    if (isLoading) return <div>loading...</div>
     return (
         <>
             {props.header}
             <WidthWrapper>
                 <div className='mt-3 gap-4 sm:grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5'>
                     {
-                        currentChildren.map(
+                        data.map(
                             child =>
                                 <ChildCardMinimized
                                     key = {child.id}
                                     isArchived={React.isValidElement(props.header) && props.header.type === CurrentBlockHeader}
-                                    child={{
-                                        childName: child.childName,
-                                        childSurname: child.childSurname,
-                                        childLastName: child.childLastName,
-                                        id: child.id,
-                                        childPhotoLink: child.childPhotoLink
-                                    }}/>
+                                    child={child}/>
                         )
                     }
                 </div>
