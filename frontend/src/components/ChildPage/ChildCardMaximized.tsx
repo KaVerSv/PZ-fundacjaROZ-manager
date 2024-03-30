3fba37e8818bb81f5b1530a355b86881ab736716
ZakharSytoi <zakharsytoi@gmail.com>
2024-03-30 16:21:05 +0100

    child cards now connected to the backend

--- frontend/src/components/ChildPage/ChildCardMaximized.tsx
+++ frontend/src/components/ChildPage/ChildCardMaximized.tsx
@@ -1,8 +1,9 @@
-import {ChildModelMaximized, currentChildrenFull} from "../../models/ChildModelMaximized.tsx";
+import {ChildModelMaximized} from "../../models/ChildModelMaximized.tsx";
 import WidthWrapper from "../wrappers/WidthWrapper.tsx";
 import ChildInfoContainer from "./ChildInfoContainer.tsx";
 import {Link} from "react-router-dom";
-import {useEffect} from "react";
+import useSWR from "swr";
+import {BASE_API_URL} from "../../api/contst.ts";
 
 interface ChildCardProps {
     childId: string;
@@ -10,42 +11,55 @@ interface ChildCardProps {
 
 
 function ChildCardMaximized(props: ChildCardProps) {
+    const fetcher: (url: string) => Promise<ChildModelMaximized> = async (url) => {
+        const response = await fetch(url);
+        if (!response.ok) {
+            throw new Error(`API request failed with status ${response.status}`);
+        }
+        const jsonData = await response.json();
+        document.title = jsonData.first_name + ' ' + jsonData.surname;
+        return jsonData;
+    };
+    const { data, error, isLoading} = useSWR<ChildModelMaximized>(BASE_API_URL + `/children/${parseInt(props.childId)}`, fetcher);
 
-    const child: ChildModelMaximized = currentChildrenFull[parseInt(props.childId) - 1];
-    useEffect(() => {
-        document.title = child.firstName + ' ' + child.surname;
-    }, []);
+    const child: ChildModelMaximized = data;
 
+
+
+    if (error) return <div>failed to load</div>
+    if (isLoading) return <div>loading...</div>
     return (
         <div className=''>
             <WidthWrapper>
                 <div
                     className='flex flex-col lg:flex-row gap-4 border-main_red border-4 p-4 rounded-2xl justify-center'>
                     <div className='flex md:block justify-center'>
-                        <img className='pb-2 w-56 sm:w-72 lg:w-80 rounded-2xl' src={child.photoPath}
+                        <img className='pb-2 w-56 sm:w-72 lg:w-80 rounded-2xl'
+                             src={child.photo_path ? child.photo_path : 'profilowe.png'}
                              alt='profilePhoto'/>
                     </div>
                     <div className='flex flex-col mt-4 gap-7'>
                         <div className='flex flex-row gap-1.5 mb-5'>
-                            <span className='text-2xl font-bold'>{child.firstName}</span>
-                            {child.secondName && <span className='text-2xl font-bold'>{child.secondName}</span>}
+                            <span className='text-2xl font-bold'>{child.first_name}</span>
+                            {child.second_name && <span className='text-2xl font-bold'>{child.second_name}</span>}
                             <span className='text-2xl font-bold'>{child.surname}</span>
                         </div>
                         {/*<div>*/}
 
                         {/*</div>*/}
                         <div className='flex flex-col gap-7 sm:grid xl:gap-4 sm:grid-cols-2 xl:grid-cols-3'>
-                            <ChildInfoContainer note='Płeć' text={child.gender === "male" ? 'Mężczyzna' : 'Kobieta'}/>
-                            <ChildInfoContainer note='Data urodzenia' text={child.birthDate}/>
-                            <ChildInfoContainer note='Miejsce urodzeina' text={child.birthPlace}/>
+                            {/*<ChildInfoContainer note='Płeć' text={child.gender === "male" ? 'Mężczyzna' : 'Kobieta'}/>*/}
+                            <ChildInfoContainer note='Płeć' text={'Mężczyzna'}/>
+                            <ChildInfoContainer note='Data urodzenia' text={child.birth_date}/>
+                            <ChildInfoContainer note='Miejsce urodzeina' text={child.birthplace}/>
                         </div>
                         <div className='flex flex-col gap-7'>
-                            <ChildInfoContainer note='Adres zamieszkania' text={child.residentialAddress}/>
-                            <ChildInfoContainer note='Adres zamieldowania' text={child.registeredAddress}/>
+                            <ChildInfoContainer note='Adres zamieszkania' text={child.residential_address}/>
+                            <ChildInfoContainer note='Adres zamieldowania' text={child.registered_address}/>
                         </div>
                         <div className='flex flex-col sm:flex-row gap-7'>
-                            <ChildInfoContainer note='Data przyjęcia' text={child.admissionDate}/>
-                            <ChildInfoContainer note='Data opuszczenia' text={child.leavingDate}/>
+                            <ChildInfoContainer note='Data przyjęcia' text={child.admission_date}/>
+                            <ChildInfoContainer note='Data opuszczenia' text={child.leaving_date}/>
                         </div>
                     </div>
                 </div>
--- frontend/src/components/ChildrenBlock/ChildCardMinimized.tsx
+++ frontend/src/components/ChildrenBlock/ChildCardMinimized.tsx
@@ -1,5 +1,5 @@
-import {ChildModelMinimized} from "../../models/ChildModelMinimized.tsx";
 import {Link} from "react-router-dom";
+import {ChildModelMinimized} from "../../models/ChildModelMinimized.tsx";
 
 interface ChildProps {
     isArchived: boolean,
@@ -8,14 +8,14 @@ interface ChildProps {
 
 // 'src/assets/2.jpg'
 function ChildCardMinimized(props: ChildProps) {
-    let optionalStyles = props.isArchived ?
+    const optionalStyles = props.isArchived ?
         `bg-main_red hover:bg-red_selected` :
         `bg-main_grey hover:bg-grey_selected`;
     return (
         <Link to={`children/${props.child.id}`} className={`mx-auto my-2 sm:m-0 flex max-w-72 h-16 rounded-xl hover:cursor-pointer ${optionalStyles}`}>
-            <img className='ml-5 p-0.5 rounded-full' src={props.child.childPhotoLink}/>
+            <img className='ml-5 p-0.5 rounded-full' src={props.child.photo_path} alt=''/>
             <span
-                className='flex items-center ml-2 text-main_white'>{props.child.childName} {props.child.childLastName} {props.child.childSurname}</span>
+                className='flex items-center ml-2 text-main_white'>{props.child.first_name} {props.child.second_name} {props.child.surname}</span>
         </Link>
     );
 }
