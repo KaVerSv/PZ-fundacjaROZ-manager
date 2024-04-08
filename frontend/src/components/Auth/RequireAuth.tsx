662ab16b95d0ba2015e51b0737aaf8d10548259f
ZakharSytoi <zakharsytoi@gmail.com>
2024-04-08 14:10:07 +0200

    Implemented RequireAuth component

---
+++ frontend/src/components/Auth/RequireAuth.tsx
@@ -0,0 +1,15 @@
+import useAuth from "../../hooks/useAuth.ts";
+import {Navigate, Outlet, useLocation} from "react-router-dom";
+
+const RequireAuth = () =>{
+    const { auth } = useAuth();
+    const location = useLocation();
+
+    return(
+        auth?.token ?
+            <Outlet/> :
+            <Navigate to={'/login'} state={{from: location}} replace/>
+    )
+}
+
+export default RequireAuth;
