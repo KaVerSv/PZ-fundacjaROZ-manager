7e77272f9010d7b2e9a3929d01eff63581d38ddb
ZakharSytoi <zakharsytoi@gmail.com>
2024-04-10 22:06:51 +0200

    add textarea support for shared form input

--- frontend/src/components/common/FormInput.tsx
+++ frontend/src/components/common/FormInput.tsx
@@ -18,11 +18,20 @@ function FormInput({name, type, label, labelColor, placeholder, register, rules,
     if (type === "date") alwaysShowLabels = true;
         return (
             <InputWrapper labelFor={name} labelNote={label} error={error} alwaysShowLabel={alwaysShowLabels} labelColor={labelColor}>
-                <input
-                    className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-                    type={type} id={name}
-                    placeholder={placeholder}
-                    {...register(name, rules)} />
+                {
+                    type === "textarea" ?
+                        <textarea
+                            className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                            id={name}
+                            placeholder={placeholder}
+                            {...register(name, rules)} />
+                        :
+                        <input
+                            className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                            type={type} id={name}
+                            placeholder={placeholder}
+                            {...register(name, rules)} />
+                }
             </InputWrapper>
 
         );
