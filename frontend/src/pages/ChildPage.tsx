7bb85e96e0ec4ac4fffb5c03bd7fbcbeccb2ad7b
ZakharSytoi <zakharsytoi@gmail.com>
2024-04-10 07:56:04 +0200

    add redirect on child page after redirect

--- frontend/src/pages/ChildPage.tsx
+++ frontend/src/pages/ChildPage.tsx
@@ -3,6 +3,7 @@ import Header from "../components/Header/Header.tsx";
 import HeightWrapper from "../components/wrappers/HeightWrapper.tsx";
 import ChildCardMaximized from "../components/ChildPage/ChildCardMaximized.tsx";
 import {useParams} from "react-router-dom";
+import NotesSection from "../components/Notes/NotesSection.tsx";
 
 function ChildPage() {
     let { id } = useParams();
@@ -12,7 +13,11 @@ function ChildPage() {
             <Top headerHeight={350}>
                 <Header/>
             </Top>
-            <ChildCardMaximized childId={id}/>
+            <div className='flex flex-col sm:flex-row justify-center'>
+                <ChildCardMaximized childId={id}/>
+                <NotesSection/>
+            </div>
+
             <div className='mt-96'></div>
         </HeightWrapper>
     );
