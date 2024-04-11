c37b239cd7e32419a33f4ee36d8fb1077d01cd6c
ZakharSytoi <zakharsytoi@gmail.com>
2024-04-11 15:18:40 +0200

    implement update handling

--- frontend/src/components/Notes/Note.tsx
+++ frontend/src/components/Notes/Note.tsx
@@ -1,17 +1,21 @@
 import {NoteModel} from "../../models/NoteModel.ts";
-import {faPencil, faTrashCan} from "@fortawesome/free-solid-svg-icons";
+import {faPencil, faReplyAll, faTrashCan} from "@fortawesome/free-solid-svg-icons";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {BASE_API_URL} from "../../api/contst.ts";
+import {useState} from "react";
+import NoteForm from "./NoteForm.tsx";
+import {Mode} from "./Mode.ts";
 
 interface NoteProps {
-    note?: NoteModel;
+    note: NoteModel;
     toggleReload: () => void;
 }
 
 function Note(props: NoteProps) {
+    const [editMode, setEditMode] = useState(false);
     const handleDeleteChild = async () => {
         try {
-            const response = await fetch(`${BASE_API_URL}/children/${props.note.child_id}/notes/${props.note.id}/`,{
+            const response = await fetch(`${BASE_API_URL}/children/${props.note.child_id}/notes/${props.note.id}/`, {
                 method: 'DELETE',
                 headers: {
                     'Authorization': `Bearer ${localStorage.getItem("token")}`
@@ -25,27 +29,45 @@ function Note(props: NoteProps) {
             console.error('Error deleting:', error);
         }
     }
-
     return (
-        <div className='bg-orange-500 bg-opacity-60 p-3 rounded-xl'>
-            <div className='flex flex-row justify-between'>
-                <span className='font-bold w-[80%]'>{props.note.title}</span>
-                <div className='flex gap-2'>
-                    <div className='text-main_red hover:text-red-500 cursor-pointer'>
-                        <FontAwesomeIcon icon={faPencil} />
+        <div className={`bg-orange-500 bg-opacity-60 ${!editMode?'p-3': '' } rounded-xl`}>
+            <div className='flex justify-between' style={editMode ? {flexDirection: "column"} : {flexDirection: "row"}}>
+                {!editMode && <span className='font-bold w-[80%]'>{props.note.title}</span>}
+                {!editMode ?
+                    <div className='flex gap-2 justify-end'>
+                        <div className='text-main_red hover:text-red-500 cursor-pointer'
+                             onClick={() => setEditMode(!editMode)}>
+                            <FontAwesomeIcon icon={faPencil}/>
+                        </div>
+                        <div className='text-main_grey hover:text-gray-900 cursor-pointer'
+                             onClick={handleDeleteChild}>
+                            <FontAwesomeIcon icon={faTrashCan}/>
+                        </div>
                     </div>
-                    <div className='text-main_grey hover:text-gray-900 cursor-pointer'
-                         onClick={handleDeleteChild}>
-                        <FontAwesomeIcon icon={faTrashCan} />
+                    :
+                    <div className='flex gap-2 justify-end p-3'>
+                        <div className='text-main_grey hover:text-gray-900 cursor-pointer'
+                             onClick={()=>setEditMode(false)}>
+                            <FontAwesomeIcon icon={faReplyAll} />
+                        </div>
                     </div>
-
-                </div>
+                }
+                {editMode &&
+                    <NoteForm toggleReload={props.toggleReload}
+                              toggleShowForm={() => {
+                                  setEditMode(false)
+                              }}
+                              mode={Mode.edit}
+                              childId={props.note.id}
+                              note={props.note}
+                    />
+                }
             </div>
-            <div>
+            {!editMode && <div>
                 <p>
                     {props.note.contents}
                 </p>
-            </div>
+            </div>}
         </div>
     );
 }
