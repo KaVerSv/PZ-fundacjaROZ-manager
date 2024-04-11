c92f79b9d5d65f96f2b21af846326ddb8bd554eb
ZakharSytoi <zakharsytoi@gmail.com>
2024-04-11 15:19:30 +0200

    return to not resizable textarea

--- frontend/src/components/common/FormInput.tsx
+++ frontend/src/components/common/FormInput.tsx
@@ -1,7 +1,5 @@
 import {FieldError, RegisterOptions, UseFormRegister} from "react-hook-form";
 import InputWrapper from "../ChildCreationForm/InputWrapper.tsx";
-import useAutosizeTextArea from "../../hooks/AutoSizeTextArea.ts";
-import {useRef, useState} from "react";
 
 interface FormProps {
     name: string;
@@ -15,34 +13,23 @@ interface FormProps {
     defaultValue?: string;
 }
 
-function FormInput({name, type, label, labelColor, placeholder, register, rules, error, defaultValue}: FormProps) {
+function FormInput({name, type, label, labelColor, placeholder, register, rules, error}: FormProps) {
     if (!placeholder) placeholder = label;
     let alwaysShowLabels: boolean = false;
     if (type === "date") alwaysShowLabels = true;
 
-    const textAreaRef = useRef<HTMLTextAreaElement>(null);
-    const[value, setValue] = useState('');
-    useAutosizeTextArea(textAreaRef.current, value);
 
-    const handleChange = (evt: React.ChangeEvent<HTMLTextAreaElement>) => {
-        const val = evt.target?.value;
-
-        setValue(val);
-    };
     return (
         <InputWrapper labelFor={name} labelNote={label} error={error} alwaysShowLabel={alwaysShowLabels}
                       labelColor={labelColor}>
             {
                 type === "textarea" ?
                     <textarea
-                        className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                        className="appearance-none block h-36 w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
                         id={name}
                         placeholder={placeholder}
                         {...register(name, rules)}
-                        onChange={handleChange}
-                        ref={textAreaRef}
-                        rows={1}
-                        value={value}/>
+                    />
                     :
                     <input
                         className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
