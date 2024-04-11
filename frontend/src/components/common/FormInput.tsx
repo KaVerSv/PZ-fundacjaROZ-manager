f2e8b307251d552e1e7952b5e46c480cf9b31616
ZakharSytoi <zakharsytoi@gmail.com>
2024-04-11 14:25:19 +0200

    implement textarea autoresizing

--- frontend/src/components/common/FormInput.tsx
+++ frontend/src/components/common/FormInput.tsx
@@ -1,5 +1,7 @@
 import {FieldError, RegisterOptions, UseFormRegister} from "react-hook-form";
 import InputWrapper from "../ChildCreationForm/InputWrapper.tsx";
+import useAutosizeTextArea from "../../hooks/AutoSizeTextArea.ts";
+import {useRef, useState} from "react";
 
 interface FormProps {
     name: string;
@@ -10,31 +12,47 @@ interface FormProps {
     error?: FieldError;
     register: UseFormRegister<any>;
     rules?: RegisterOptions;
+    defaultValue?: string;
 }
 
-function FormInput({name, type, label, labelColor, placeholder, register, rules, error}: FormProps) {
+function FormInput({name, type, label, labelColor, placeholder, register, rules, error, defaultValue}: FormProps) {
     if (!placeholder) placeholder = label;
     let alwaysShowLabels: boolean = false;
     if (type === "date") alwaysShowLabels = true;
-        return (
-            <InputWrapper labelFor={name} labelNote={label} error={error} alwaysShowLabel={alwaysShowLabels} labelColor={labelColor}>
-                {
-                    type === "textarea" ?
-                        <textarea
-                            className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-                            id={name}
-                            placeholder={placeholder}
-                            {...register(name, rules)} />
-                        :
-                        <input
-                            className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-                            type={type} id={name}
-                            placeholder={placeholder}
-                            {...register(name, rules)} />
-                }
-            </InputWrapper>
 
-        );
+    const textAreaRef = useRef<HTMLTextAreaElement>(null);
+    const[value, setValue] = useState('');
+    useAutosizeTextArea(textAreaRef.current, value);
+
+    const handleChange = (evt: React.ChangeEvent<HTMLTextAreaElement>) => {
+        const val = evt.target?.value;
+
+        setValue(val);
+    };
+    return (
+        <InputWrapper labelFor={name} labelNote={label} error={error} alwaysShowLabel={alwaysShowLabels}
+                      labelColor={labelColor}>
+            {
+                type === "textarea" ?
+                    <textarea
+                        className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                        id={name}
+                        placeholder={placeholder}
+                        {...register(name, rules)}
+                        onChange={handleChange}
+                        ref={textAreaRef}
+                        rows={1}
+                        value={value}/>
+                    :
+                    <input
+                        className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                        type={type} id={name}
+                        placeholder={placeholder}
+                        {...register(name, rules)} />
+            }
+        </InputWrapper>
+
+    );
 }
 
 export default FormInput;
