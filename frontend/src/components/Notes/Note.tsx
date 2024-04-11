2c1ed360d0558303e2d4479a6c2bcceff6b59360
ZakharSytoi <zakharsytoi@gmail.com>
2024-04-11 11:57:57 +0200

    implement delete handling

--- frontend/src/components/Notes/Note.tsx
+++ frontend/src/components/Notes/Note.tsx
@@ -1,12 +1,31 @@
 import {NoteModel} from "../../models/NoteModel.ts";
 import {faPencil, faTrashCan} from "@fortawesome/free-solid-svg-icons";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {BASE_API_URL} from "../../api/contst.ts";
 
 interface NoteProps {
     note?: NoteModel;
+    toggleReload: () => void;
 }
 
 function Note(props: NoteProps) {
+    const handleDeleteChild = async () => {
+        try {
+            const response = await fetch(`${BASE_API_URL}/children/${props.note.child_id}/notes/${props.note.id}/`,{
+                method: 'DELETE',
+                headers: {
+                    'Authorization': `Bearer ${localStorage.getItem("token")}`
+                }
+            })
+            if (!response.ok) console.error('Failed to delete:', response.statusText);
+            else {
+                props.toggleReload();
+            }
+        } catch (error) {
+            console.error('Error deleting:', error);
+        }
+    }
+
     return (
         <div className='bg-orange-500 bg-opacity-60 p-3 rounded-xl'>
             <div className='flex flex-row justify-between'>
@@ -15,7 +34,8 @@ function Note(props: NoteProps) {
                     <div className='text-main_red hover:text-red-500 cursor-pointer'>
                         <FontAwesomeIcon icon={faPencil} />
                     </div>
-                    <div className='text-main_grey hover:text-gray-900 cursor-pointer'>
+                    <div className='text-main_grey hover:text-gray-900 cursor-pointer'
+                         onClick={handleDeleteChild}>
                         <FontAwesomeIcon icon={faTrashCan} />
                     </div>
 
