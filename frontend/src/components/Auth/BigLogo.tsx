559e9c002818d8d92ad2428b494336cab6598d6d
ZakharSytoi <zakharsytoi@gmail.com>
2024-04-16 19:51:26 +0200

    fix no logo in docker bug

--- frontend/src/components/Auth/BigLogo.tsx
+++ frontend/src/components/Auth/BigLogo.tsx
@@ -1,4 +1,3 @@
-import logo from "../../../public/logo.png";
 import {Link} from "react-router-dom";
 
 function BigLogo() {
@@ -6,7 +5,7 @@ function BigLogo() {
         <div className='flex h-full items-center'>
             <Link to={'/'}>
                 <div className='flex flex-col items-center'>
-                    <img className='w-24 h-auto lg:w-64' style={{paddingBottom: '4px'}} src={logo} alt="Logo"/>
+                    <img className='w-24 h-auto lg:w-64' style={{paddingBottom: '4px'}} src='logo.png' alt="Logo"/>
                     <span className='font-oswald uppercase text-3xl text-main_red hidden sm:flex items-end'> rozwój opieka zaufanie</span>
                 </div>
             </Link>
