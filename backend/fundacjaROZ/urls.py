5191ec7d87ba9f066dfb262079c96aa0232fcfc7
Kondro571 <kondziot2000@gmail.com>
2024-04-24 18:17:03 +0200

    cos

--- backend/fundacjaROZ/urls.py
+++ backend/fundacjaROZ/urls.py
@@ -13,11 +13,15 @@ urlpatterns = [
  	path('children/<int:pk>/relatives/', ChildrenRelativesAPIView.as_view()),
     path('children/<int:pk>/relatives/<int:relative_id>/', ChildrenRelativesDetailsAPIView.as_view()),
     
+    path('relatives/<int:pk>/children/', RelativeChildrensAPIView.as_view()),
+    path('relatives/<int:pk>/children/<int:child_id>/', RelativeChildrensDetailsAPIView.as_view()),
+    
     path('children/<int:pk>/notes/', ChildrenNotesAPIView.as_view()),
     path('children/<int:pk>/notes/<int:note_id>/', ChildrenNotesDetailsAPIView().as_view()),
     
 	path('children/<int:pk>/documents/',  ChildrenDocumentsAPIView.as_view()),
     path('children/<int:pk>/documents/<int:document_id>/',  ChildrenDocumentsDetailsAPIView().as_view()),
+    
     
 	path('children/<int:pk>/photo/', ChildrenPhotoAPIView.as_view()),
 ]
