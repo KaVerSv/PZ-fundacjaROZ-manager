0bb26a97a53d477466d90ee32ccf7a828cad4ac1
ZakharSytoi <zakharsytoi@gmail.com>
2024-05-04 14:17:52 +0200

    integrate new sorting into child block component

--- frontend/src/components/ChildrenBlock/ChildrenBlock.tsx
+++ frontend/src/components/ChildrenBlock/ChildrenBlock.tsx
@@ -7,11 +7,55 @@ import {ChildModelMinimized} from "../../models/ChildModelMinimized.tsx";
 import {BASE_API_URL} from "../../api/contst.ts";
 
 interface ChildBlockProps {
-    header: React.ReactNode
+    header: React.ReactNode;
+    sortBy: string;
 }
 
 
 function ChildrenBlock(props: ChildBlockProps) {
+    let currentSortingFn: (a: ChildModelMinimized, b: ChildModelMinimized) => number;
+    switch (props.sortBy) {
+        case "name": {
+            currentSortingFn = (a, b) => a.first_name.localeCompare(b.first_name);
+            break;
+        }
+        case "nameReversed": {
+            currentSortingFn = (a, b) => b.first_name.localeCompare(a.first_name);
+            break;
+        }
+        case "surname": {
+            currentSortingFn = (a, b) => a.surname.localeCompare(b.surname);
+            break;
+        }
+        case "surnameReversed": {
+            currentSortingFn = (a, b) => b.surname.localeCompare(a.surname);
+            break;
+        }
+        case "birthDate": {
+            currentSortingFn = (a, b) => +new Date(a.birth_date) - +new Date(b.birth_date);
+            break;
+        }
+        case "birthDateReversed": {
+            currentSortingFn = (a, b) => +new Date(b.birth_date) - +new Date(a.birth_date);
+            break;
+        }
+        case "admissionDate": {
+            currentSortingFn = (a, b) => +new Date(a.admission_date) - +new Date(b.admission_date);
+            break;
+        }
+        case "admissionDateReversed": {
+            currentSortingFn = (a, b) => +new Date(b.admission_date) - +new Date(a.admission_date);
+            break;
+        }
+        case "leavingDate": {
+            currentSortingFn = (a, b) => +new Date(a.leaving_date) - +new Date(b.leaving_date);
+            break;
+        }
+        case "leavingDateReversed": {
+            currentSortingFn = (a, b) => +new Date(b.leaving_date) - +new Date(a.leaving_date);
+            break;
+        }
+    }
     const fetcher: (url: string) => Promise<ChildModelMinimized[]> = async (url) => {
         const token = localStorage.getItem("token");
 
@@ -30,7 +74,7 @@ function ChildrenBlock(props: ChildBlockProps) {
     };
 
     const {data, error, isLoading} = useSWR<ChildModelMinimized[]>(BASE_API_URL +
-        `children/${React.isValidElement(props.header) && props.header.type === CurrentBlockHeader ? 'current/' : 'archival/'}`, fetcher, {refreshInterval:0});
+        `children/${React.isValidElement(props.header) && props.header.type === CurrentBlockHeader ? 'current/' : 'archival/'}`, fetcher, {refreshInterval: 0});
 
     if (error) return <div>failed to load</div>
     if (isLoading) return <div>loading...</div>
@@ -40,13 +84,14 @@ function ChildrenBlock(props: ChildBlockProps) {
             <WidthWrapper>
                 <div className='mt-3 gap-4 sm:grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5'>
                     {
-                        data.map(
-                            child =>
-                                <ChildCardMinimized
-                                    key={child.id}
-                                    isArchived={React.isValidElement(props.header) && props.header.type === CurrentBlockHeader}
-                                    child={child}/>
-                        )
+                        data.sort((a, b) => currentSortingFn(a, b))
+                            .map(
+                                child =>
+                                    <ChildCardMinimized
+                                        key={child.id}
+                                        isArchived={React.isValidElement(props.header) && props.header.type === CurrentBlockHeader}
+                                        child={child}/>
+                            )
                     }
                 </div>
             </WidthWrapper>
