814b5fdfeb8cbec16a5609f693d86fab022d4784
ZakharSytoi <zakharsytoi@gmail.com>
2024-05-04 14:16:47 +0200

    integrate searching type input into child block headers

--- frontend/src/components/ChildrenBlock/childBlockHeaders/ArchiveBlockHeader.tsx
+++ frontend/src/components/ChildrenBlock/childBlockHeaders/ArchiveBlockHeader.tsx
@@ -1,11 +1,13 @@
 import WidthWrapper from "../../wrappers/WidthWrapper.tsx";
+import SortingTypeChoice from "../SortingTypeChoice.tsx";
 
-function ArchiveBlockHeader() {
+function ArchiveBlockHeader(props: {onSortingMethodChange : (sortingMethod: string)=>void;}) {
     return (
         <div className='flex bg-main_grey mt-2 items-center' style={{minHeight: '44px'}}>
             <WidthWrapper>
                 <div className='flex items-center justify-between'>
                     <span className='text-main_white'>Archiwalni wychowankowie</span>
+                    <SortingTypeChoice isArch={true} onSortingMethodChange={props.onSortingMethodChange}/>
                 </div>
             </WidthWrapper>
         </div>
