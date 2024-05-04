cf9df80fda62641cc06fe1b3224118a64fcc9dd1
ZakharSytoi <zakharsytoi@gmail.com>
2024-05-04 14:17:43 +0200

    integrate new sorting into home component

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -4,19 +4,35 @@ import HeightWrapper from "../components/wrappers/HeightWrapper.tsx";
 import ChildrenBlock from "../components/ChildrenBlock/ChildrenBlock.tsx";
 import CurrentBlockHeader from "../components/ChildrenBlock/childBlockHeaders/CurrentBlockHeader.tsx";
 import ArchiveBlockHeader from "../components/ChildrenBlock/childBlockHeaders/ArchiveBlockHeader.tsx";
-import {useEffect} from "react";
+import {useEffect, useState} from "react";
 
 const Home = () => {
     useEffect(() => {
         document.title = "Strona głowna";
     }, []);
+    const [currentChildrenSortingMethod, setCurrentChildrenSortingMethod] = useState('name');
+    const [archivalChildrenSortingMethod, setArchivalChildrenSortingMethod] = useState('name');
+
+
+    const handleCurrentChildrenSortingMethodChange = (method: string) => {
+        setCurrentChildrenSortingMethod(method);
+    };
+
+    const handleArchivalChildrenSortingMethodChange = (method: string) => {
+        setArchivalChildrenSortingMethod(method);
+    };
+
     return (
         <HeightWrapper>
             <Top headerHeight={300}>
                 <Header/>
             </Top>
-            <ChildrenBlock header={<CurrentBlockHeader/>}/>
-            <ChildrenBlock header={<ArchiveBlockHeader/>}/>
+            <ChildrenBlock sortBy={currentChildrenSortingMethod}
+                           header={<CurrentBlockHeader
+                               onSortingMethodChange={handleCurrentChildrenSortingMethodChange}/>}/>
+            <ChildrenBlock sortBy={archivalChildrenSortingMethod}
+                           header={<ArchiveBlockHeader
+                               onSortingMethodChange={handleArchivalChildrenSortingMethodChange}/>}/>
         </HeightWrapper>
 
 
