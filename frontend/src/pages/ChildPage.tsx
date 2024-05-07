d74f1f7cd6505d162e2ab5380257785cce87f0c6
ZakharSytoi <zakharsytoi@gmail.com>
2024-05-07 18:18:04 +0200

    integrate document section into child page

--- frontend/src/pages/ChildPage.tsx
+++ frontend/src/pages/ChildPage.tsx
@@ -6,6 +6,7 @@ import {useParams} from "react-router-dom";
 import NotesSection from "../components/ChildPage/Notes/NotesSection.tsx";
 import RelativesSection from "../components/ChildPage/Relatives/RelativesSection.tsx";
 import SchoolsSection from "../components/ChildPage/Schools/SchoolsSection.tsx";
+import DocumentSection from "../components/ChildPage/Documents/DocumentSection.tsx";
 
 function ChildPage() {
     let {id} = useParams();
@@ -16,7 +17,11 @@ function ChildPage() {
                 <Header/>
             </Top>
             <div className='flex flex-col sm:flex-row justify-center'>
-                <ChildCardMaximized childId={id}/>
+                <div className={`flex flex-col`}>
+                    <ChildCardMaximized childId={id}/>
+                    <DocumentSection childId={id}/>
+                </div>
+
                 <div className='flex flex-col justify-center' style={{alignItems: "center"}}>
                     <SchoolsSection childId={id}/>
                     <NotesSection childId={id}/>
