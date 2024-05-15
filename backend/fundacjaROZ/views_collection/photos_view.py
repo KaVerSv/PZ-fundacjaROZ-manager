02424b8e644c3d4746c5a8034a082dedd7efe3a7
zywczak <123404938+zywczak@users.noreply.github.com>
2024-05-15 11:07:37 +0200

    google drive
    all delete post i put actions is on google drive

--- backend/fundacjaROZ/views_collection/photos_view.py
+++ backend/fundacjaROZ/views_collection/photos_view.py
@@ -1,10 +1,9 @@
+import tempfile
 import time
 from django.shortcuts import get_object_or_404
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from django.http import HttpResponse
 from ..models import Children
-from rest_framework import status
 from django.conf import settings
 
 from googleapiclient import discovery
@@ -15,6 +14,7 @@ import os
 from django.http import HttpResponse
 from rest_framework import status
 from googleapiclient.errors import HttpError
+from googleapiclient.http import MediaFileUpload
 
 class ChildrenPhotoAPIView(APIView):
     def get(self, request, pk):
@@ -51,13 +51,29 @@ class ChildrenPhotoAPIView(APIView):
         except HttpError as e:
             return HttpResponse(f"Wystąpił błąd podczas pobierania pliku {e}")
 
-
     def delete(self, request, pk):
         child = get_object_or_404(Children, pk=pk)
         if child.photo_path:
-            file_path = os.path.join(settings.MEDIA_ROOT, child.photo_path)
-            if os.path.exists(file_path):
-                os.remove(file_path)
+            SCOPES = 'https://www.googleapis.com/auth/drive'
+            file_path_store = os.path.join(settings.GOOGLE_ROOT, 'storage.json')
+            store = file.Storage(file_path_store)
+            creds = store.get()
+            if not creds or creds.invalid:
+                file_path = os.path.join(settings.GOOGLE_ROOT, 'credentials.json')
+                flow = client.flow_from_clientsecrets(file_path, SCOPES)
+                creds = tools.run_flow(flow, store)
+            DRIVE = discovery.build('drive', 'v3', http=creds.authorize(Http()))
+
+            query = f"name='{child.photo_path}'"
+            try:
+                response = DRIVE.files().list(q=query, fields='files(id)').execute()
+                files = response.get('files', [])
+                if files:
+                    file_id = files[0]['id']
+                    DRIVE.files().delete(fileId=file_id).execute()
+            except HttpError as e:
+                return Response({'error': f'Wystąpił błąd podczas usuwania zdjecia: {e}'}, status=status.HTTP_500_INTERNAL_SERVER_ERROR) 
+            
             child.photo_path = ""
             child.save()
             return Response(status=status.HTTP_204_NO_CONTENT)
@@ -66,28 +82,54 @@ class ChildrenPhotoAPIView(APIView):
         
     def put(self, request, pk):
         child = get_object_or_404(Children, pk=pk)
-        if child.photo_path:
-            file_path = os.path.join(settings.MEDIA_ROOT, child.photo_path)
-            if os.path.exists(file_path):
-                os.remove(file_path)
-
         if 'photo' in request.FILES:
-            photo = request.FILES['photo']
+            SCOPES = 'https://www.googleapis.com/auth/drive'
+            file_path_store = os.path.join(settings.GOOGLE_ROOT, 'storage.json')
+            store = file.Storage(file_path_store)
+            creds = store.get()
+            if not creds or creds.invalid:
+                file_path = os.path.join(settings.GOOGLE_ROOT, 'credentials.json')
+                flow = client.flow_from_clientsecrets(file_path, SCOPES)
+                creds = tools.run_flow(flow, store)
+            DRIVE = discovery.build('drive', 'v3', http=creds.authorize(Http()))
 
-            filename = photo.name                
-            if os.path.exists(os.path.join(settings.MEDIA_ROOT, filename)):
-                name, extension = os.path.splitext(filename)
-                timestamp = int(time.time() * 1000)
-                filename = f"{name}_{timestamp}{extension}"
+            if child.photo_path:
+                query = f"name='{child.photo_path}'"
+                try:
+                    response = DRIVE.files().list(q=query, fields='files(id)').execute()
+                    files = response.get('files', [])
+                    if files:
+                        file_id = files[0]['id']
+                        DRIVE.files().delete(fileId=file_id).execute()
+                except HttpError as e:
+                    return Response({'error': f'Wystąpił błąd podczas usuwania zdjecia: {e}'}, status=status.HTTP_500_INTERNAL_SERVER_ERROR) 
             
-            if hasattr(photo, 'content_type') and photo.content_type in ['image/jpeg', 'image/png']:
-                with open(os.path.join(settings.MEDIA_ROOT, filename), 'wb') as destination:
-                    for chunk in photo.chunks():
-                        destination.write(chunk)
+                child.photo_path = ""
+
+            photo = request.FILES['photo']
+            filename = photo.name   
+
+            query = f"name='{filename}'"
+            response = DRIVE.files().list(q=query, fields='files(id)').execute()
+            files = response.get('files', [])
+            if files:
+                base_name, extension = os.path.splitext(filename)
+                timestamp = int(time.time() * 1000)
+                filename = f"{base_name}_{timestamp}{extension}"
+
+            file_metadata = {
+                'name': filename,
+                'parents': ['1FOkTJQyVAXMbryg7PNDltFLLVLeQVsK6']
+            }
+            with tempfile.NamedTemporaryFile(delete=False) as temp_file:
+                temp_file.write(photo.read())
+                media = MediaFileUpload(temp_file.name, mimetype='application/octet-stream', resumable=True)
+            try:
+                DRIVE.files().create(body=file_metadata, media_body=media, fields='id').execute()
                 child.photo_path = filename
                 child.save()
-                return Response({'message': 'Zdjęcie zostało zaktualizowane'}, status=status.HTTP_200_OK)
-            else:
-                return Response({'error': 'Nieobsługiwany typ pliku'}, status=status.HTTP_415_UNSUPPORTED_MEDIA_TYPE)
+            except:
+                return Response({'error': 'Wystąpił błąd podczas zapisywania pliku na dysku Google'}, status=status.HTTP_415_UNSUPPORTED_MEDIA_TYPE)
+            return Response({'message': 'Zdjęcie zostało zaktualizowane'}, status=status.HTTP_200_OK)
         else:
             return Response({'error': 'Nie przesłano zdjęcia'}, status=status.HTTP_400_BAD_REQUEST)
