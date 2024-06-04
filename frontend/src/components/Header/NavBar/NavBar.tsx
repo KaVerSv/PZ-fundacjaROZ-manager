c9da671c6ca6f8de44da5030f451e0e5e5568c3e
ZakharSytoi <zakharsytoi@gmail.com>
2024-06-04 23:28:36 +0200

    add main page to nav

--- frontend/src/components/Header/NavBar/NavBar.tsx
+++ frontend/src/components/Header/NavBar/NavBar.tsx
@@ -1,14 +1,15 @@
 import NavItem from "./NavItem.tsx";
 import WidthWrapper from "../../wrappers/WidthWrapper.tsx";
+import {NavLink} from "react-router-dom";
 
 function NavBar() {
     return (
         <div className='bg-main_red'>
             <WidthWrapper>
                 <nav className='flex gap-4'>
-                    <NavItem></NavItem>
-                    <NavItem></NavItem>
-                    <NavItem></NavItem>
+                    <NavLink to={'/'}><NavItem navText="Strona główna"></NavItem></NavLink>
+                    <NavItem navText="Jakaś strona"></NavItem>
+                    <NavItem navText="Strony nie ma"></NavItem>
                 </nav>
 
             </WidthWrapper>
